e3c2efa0c3b5d0666c4d5570ff0cea078395a1be
Chough <carter.hough@hotmail.com>
2018-08-31 14:18:08 -0600

    Initial commit

---
+++ wertyu.rb
@@ -0,0 +1,8 @@
+#!/usr/bin/ruby -w
+
+# Carter Hough
+# 08/31/18
+# CSCI 480
+# Dr. MacEvoy
+
+# This program is a soltuion to the Kattis problem 'WERTYU' which can be found at https://open.kattis.com/problems/wertyu
