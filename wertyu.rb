c0f31be79f238b064c90239ccd3bf37592ecf743
Chough <carter.hough@hotmail.com>
2018-08-31 15:15:47 -0600

    Added comments to code

--- wertyu.rb
+++ wertyu.rb
@@ -7,15 +7,19 @@
 
 # This program is a soltuion to the Kattis problem 'WERTYU' which can be found at https://open.kattis.com/problems/wertyu
 
+# shiftLeft function accepts a string in all uppercase letters, and returns a string with each character in the string shifted one key to the left on the keyboard.
 def shiftLeft(string)
+    # Ruby hash (dictionary) consisting of key->value pairs to map each applicable key to the key on its left
     dictionary = { "1" => "`", "2" => "1", "3" => "2", "4" => "3", "5" => "4", "6" => "5", "7" => "6", "8" => "7", "9" => "8", "0" => "9", "-" => "0", "=" => "-",
                 "W" => "Q", "E" => "W", "R" => "E", "T" => "R", "Y" => "T", "U" => "Y", "I" => "U", "O" => "I", "P" => "O", "[" => "P", "]" => "[", "\\" => "]",
                 "S" => "A", "D" => "S", "F" => "D", "G" => "F", "H" => "G", "J" => "H", "K" => "J", "L" => "K", ";" => "L", "\'" => ";",
                 "X" => "Z", "C" => "X", "V" => "C", "B" => "V", "N" => "B", "M" => "N", "," => "M", "." => ",", "/" => ".",
                 " " => " " }
     
+    # Splits the string into individual characters
     chars = string.split('')
     answer = ""
+    # Iterates through each character in the string, gets the corresponding value in the dictionary for that char as the key, and appends the returned value to string 'answer'
     chars.each { |c|
         answer << dictionary[c]
     }
@@ -23,6 +27,7 @@ def shiftLeft(string)
     return answer
 end    
 
+# solve function to get console input for the string, and print returned value from shiftLeft() to console
 def solve
     usr_input = gets.chomp
     puts shiftLeft(usr_input)
