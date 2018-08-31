d969e35f0715d8b237f3fea35d854b7ea022f4a4
Chough <carter.hough@hotmail.com>
2018-08-31 14:19:02 -0600

    Added initial dictionary to map each key to the key on its left.

--- wertyu.rb
+++ wertyu.rb
@@ -6,3 +6,11 @@
 # Dr. MacEvoy
 
 # This program is a soltuion to the Kattis problem 'WERTYU' which can be found at https://open.kattis.com/problems/wertyu
+
+dictionary = { "1" => "`", "2" => "1", "3" => "2", "4" => "3", "5" => "4", "6" => "5", "7" => "6", "8" => "7", "9" => "8", "0" => "9", "-" => "0", "=" => "-",
+            "W" => "Q", "E" => "W", "R" => "E", "T" => "R", "Y" => "T", "U" => "Y", "I" => "U", "O" => "I", "P" => "O", "[" => "P", "]" => "[", "\\" => "]",
+            "S" => "A", "D" => "S", "F" => "D", "G" => "F", "H" => "G", "J" => "H", "K" => "J", "L" => "K", ";" => "L", "\'" => ";",
+            "X" => "Z", "C" => "X", "V" => "C", "B" => "V", "N" => "B", "M" => "N", "," => "M", "." => ",", "/" => "." }
+
+input = gets.chomp.upcase
+puts dictionary[input]
