54b0e9f5f535592547416f65b918b53d3ce4058d
Chough <carter.hough@hotmail.com>
2018-09-02 17:15:08 -0600

    Added additional unit tests.

--- bits.rb
+++ bits.rb
@@ -15,10 +15,12 @@ end
 def solve
     numTestCases = gets.chomp.to_i
     returnedValues = []
+
     for _ in 1..numTestCases
         testCase = gets.chomp.to_i
         returnedValues.append(numOneBits(testCase))
     end
+
     puts returnedValues
 end
 
@@ -30,6 +32,20 @@ if ARGV[0] == "test"
     class TestMethods < Test::Unit::TestCase 
         def test_numOneBits_simple
             assert_equal(2, numOneBits(9))
+        end
+        def test_numOneBits_lower_bound
+            assert_equal(0, numOneBits(0))
+        end
+        def test_numOneBits_upper_bound
+            assert_equal(1, numOneBits(2**31))
+        end
+        def test_numOneBits_kattis_samples
+            assert_equal(1, numOneBits(1))
+            assert_equal(1, numOneBits(2))
+            assert_equal(2, numOneBits(3))
+            assert_equal(1, numOneBits(4))
+            assert_equal(2, numOneBits(10))
+            assert_equal(5, numOneBits(94))
         end
     end
 else
