25f4ed97f15a3497f916d0c47efd584c459291df
Chough <carter.hough@hotmail.com>
2018-09-02 16:05:13 -0600

    Initial commit

---
+++ bits.rb
@@ -0,0 +1,8 @@
+#!/usr/bin/ruby
+
+# Carter Hough
+# 09/02/18
+# CSCI 480
+# Dr. MacEvoy
+
+# This program is a solution to the Kattis problem 'bits' which can be found at https://open.kattis.com/problems/bits
