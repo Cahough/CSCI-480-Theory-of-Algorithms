7c54282bf6977f2bee8fcd2a2705f923eb09ea24
Chough <carter.hough@hotmail.com>
2018-09-01 21:03:29 -0600

    Initial commit (re-doing with new problem).

---
+++ goodmorning.rb
@@ -0,0 +1,9 @@
+#!/usr/bin/ruby
+
+# Carter Hough
+# 09/01/18
+# CSCI 480
+# Dr. MacEvoy
+
+# This program is a solution to the Kattis problem 'Good Morning!' which can be found at https://open.kattis.com/problems/goodmorning
+
